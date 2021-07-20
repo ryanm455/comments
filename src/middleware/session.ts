7dc49fa2d6528aca490627e9f8e11cd8eba21865
Ryan M <ryan.mabson@outlook.com>
2021-07-20 13:16:53 +0100

    Not secure cookie works

--- src/middleware/session.ts
+++ src/middleware/session.ts
@@ -3,7 +3,7 @@ import s from "cookie-session";
 export const session = s({
   name: "session",
   secret: process.env.TOKEN_SECRET!,
-  // secure: process.env.NODE_ENV === "production",
+  secure: process.env.NODE_ENV === "production",
   httpOnly: true,
   expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7), // 1 week
 });
