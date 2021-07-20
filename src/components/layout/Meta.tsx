8b9378755ce45ec26f7cc8cb3eca6681598b7a0e
Ryan M <ryan.mabson@outlook.com>
2021-07-20 12:34:41 +0100

    Favicon cutoff fix

--- src/components/layout/Meta.tsx
+++ src/components/layout/Meta.tsx
@@ -1,4 +1,4 @@
-import { APP_DESCRIPTION, APP_LOGO, APP_NAME, APP_URL } from "meta";
+import { APP_DESCRIPTION, APP_NAME, APP_URL } from "meta";
 import { NextSeo } from "next-seo";
 import Head from "next/head";
 import { memo } from "react";
@@ -34,7 +34,7 @@ const Meta = memo(() => (
         },
         {
           rel: "icon",
-          href: `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${APP_LOGO}</text></svg>`,
+          href: `/favicon.svg`,
         },
       ]}
       additionalMetaTags={[
