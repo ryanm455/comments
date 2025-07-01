fb858728afd1d6158ea1dd9722b00c4a8c362fda
Ryan Mabson <ryan.mabson@outlook.com>
2025-07-01 20:52:03 +0100

    fix build errors

--- src/app/(main)/dashboard/layout.tsx
+++ src/app/(main)/dashboard/layout.tsx
@@ -1,12 +1,8 @@
 import React from "react";
 
-import { User } from "@prisma/client";
-
 import { SideNav } from "../../../components/dashboard/SideNav";
 
-type Props = { user: User };
-
-const DashLayout = ({ children }: React.PropsWithChildren<Props>) => (
+const DashLayout = ({ children }: { children: React.ReactNode }) => (
   <div className="flex flex-col md:flex-row min-h-[100vh]">
     <SideNav />
     <div className="w-full overflow-auto container px-4 mx-auto mb-7 max-w-5xl">
